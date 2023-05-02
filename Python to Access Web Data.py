cf54b4cfd53a87604ec02213751f9bbc688d9e5c
emilysheen <esheen15@gmail.com>
2023-05-01 21:23:46 -0600

    Finishing Week 4

--- Python to Access Web Data.py
+++ Python to Access Web Data.py
@@ -369,3 +369,229 @@ for line in fhand:
     for word in words:
         counts[word] = counts.get(word, 0) + 1
 print(counts)
+
+
+### Example Code file: urllib1.py
+import urllib.request
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
+
+### Example Code File: urlwords.py
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
+
+### Chapter 12.5: Parsing Web Pages
+### We can write a Python program that pretends to be a browser and pulls web pages / data
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+url = input("Enter URL: ")  # http://www.dr-chuck.com/page1.htm
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+
+### Example Code File: urllinks.py
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+
+### Quiz Week 4
+# 1)  Which of the following Python data structures is most similar to the value returned in this line of Python:
+# Ans: file handle
+x = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+type(x)
+
+# 2) In this Python code, which line actually reads the data?
+# mysock.recv()
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
+
+# 3) Which of the following regular expressions would extract the URL from this line of HTML:
+# <p>Please click <a href="http://www.dr-chuck.com">here</a></p>
+# Answer: href="(.+)"
+
+# 4) In this Python code, which line is most like the open() call to read a file:
+# Answer: mysock.connect()
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
+
+# 5) Which HTTP header tells the browser the kind of document that is being returned?
+# Answer: Content-Type
+
+# 6) What should you check before scraping a web site?
+# Answer: That the website allows scraping
+
+# 7) What is the purpose of the BeautifulSoup Python library?
+# Answer: It repairs and parses HTML to make it easier for a program to understand
+
+# 8) What ends up in the "x" variable in the following code:
+# Answer:  A list of all the anchor tags (<a..) in the HTML from the URL
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+x = soup('a')
+type(x)
+print(x)
+
+# 9) What is the most common Unicode encoding when moving data between systems?
+# Answer: UTF-8
+
+# 10) What is the decimal (Base-10) numeric value for the upper case letter "G" in the ASCII character set?
+ord('G')
+# Answer: 71
+
+# 11) What word does the following sequence of numbers represent in ASCII:
+# 108, 105, 110, 101
+
+chr(108)
+chr(105)
+chr(110)
+chr(101)
+
+# Answer: line
+
+# 12) How are strings stored internally in Python 3?
+x = str("poo")
+type(x)
+type(str(u"poo"))
+# Answer: Unicode
+
+# 13) When reading data across the network (i.e. from a URL) in Python 3, what method must be used to convert it
+# to the internal format used by strings?
+# Answer: decode()
+
+
+### Week 4 Assignment:
+#  Based on the urllink2.py file, printed below:
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
+
+
+##### In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py (above)
+# The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers
+# and compute the sum of the numbers in the file.
+
+# We provide two files for this assignment. One is a sample file where we give you the sum for your testing
+# and the other is the actual data you need to process for the assignment.
+
+# Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
+# Actual data: http://py4e-data.dr-chuck.net/comments_1804705.html (Sum ends with 79)
+# You do not need to save these files to your folder since your program will read the data directly from the URL.
+# Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
+
+### DATA FORMAT
+# The file is a table of names and comment counts. You can ignore most of the data in the file except for
+# lines like the following:
+
+# <tr><td>Modu</td><td><span class="comments">90</span></td></tr>
+# <tr><td>Kenzie</td><td><span class="comments">88</span></td></tr>
+# <tr><td>Hubert</td><td><span class="comments">87</span></td></tr>
+
+# You are to find all the <span> tags in the file and pull out the numbers from the tag and sum the numbers.
+# Look at the sample code provided. It shows how to find all of a certain kind of tag,
+# loop through the tags and extract the various aspects of the tags.
+# You need to adjust this code to look for span tags and pull out the text content of the span tag,
+# convert them to integers and add them up to complete the assignment.
+
+# Sample Execution
+
+# $ python3 solution.py
+# Enter - http://py4e-data.dr-chuck.net/comments_42.html
+# Count 50
+# Sum 2...
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the span tags
+
+tags = soup('span')
+num_comments = []
+for tag in tags:
+    # Look at the parts of a tag
+    num_comments.append(int(tag.contents[0]))
+# print(num_comments)
+print("Count " + str(len(num_comments)))
+print("Sum " + str(sum(num_comments)))
