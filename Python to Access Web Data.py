f7363cf82fd1739ced79b4029bcfb7d642603113
emilysheen <esheen15@gmail.com>
2023-05-02 12:24:37 -0600

    Finished assignment 2 for week 4

--- Python to Access Web Data.py
+++ Python to Access Web Data.py
@@ -595,3 +595,116 @@ for tag in tags:
 # print(num_comments)
 print("Count " + str(len(num_comments)))
 print("Sum " + str(sum(num_comments)))
+
+
+# Week 4 Assignment 2: Following Links in Python
+
+# In this assignment you will write a Python program that expands on http://www.py4e.com/code3/urllinks.py.
+# The program will use urllib to read the HTML from the data files below, extract the href= values from the anchor tags,
+# scan for a tag that is in a particular position relative to the first name in the list,
+# follow that link and repeat the process a number of times and report the last name you find.
+
+# We provide two files for this assignment. One is a sample file where we give you the name for your testing
+# and the other is the actual data you need to process for the assignment
+
+# Sample problem: Start at http://py4e-data.dr-chuck.net/known_by_Fikret.html
+# Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times.
+# The answer is the last name that you retrieve.
+# Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
+# Last name in sequence: Anayah
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+links = [tags[2].get('href', None)]
+names = [tags[2].contents[0]]
+# for tag in tags:
+#     print(tag.get('href', None))
+#     print('TAG:', tag)
+#     print('URL:', tag.get('href', None))
+#     print('Contents:', tag.contents[0])
+#     print('Attrs:', tag.attrs)
+
+# Must click the link to get the next name 4 times
+for i in range(3):
+    print(i)
+    url = links[i]
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    links.append(tags[2].get('href', None))
+    names.append(tags[2].contents[0])
+print(links)
+print(names)
+print(names[len(names)-1])
+
+# Actual problem: Start at: http://py4e-data.dr-chuck.net/known_by_Torran.html
+# Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times.
+# The answer is the last name that you retrieve.
+# Hint: The first character of the name of the last page that you will load is: C
+# Strategy:
+# The web pages tweak the height between the links and hide the page after a few seconds to make it difficult
+# for you to do the assignment without writing a Python program. But frankly with a little effort and patience
+# you can overcome these attempts to make it a little harder to complete the assignment without writing a
+# Python program. But that is not the point. The point is to write a clever Python program to solve the program.
+#
+# Sample execution
+#
+# Here is a sample execution of a solution:
+#
+# $ python3 solution.py
+# Enter URL: http://py4e-data.dr-chuck.net/known_by_Fikret.html
+# Enter count: 4
+# Enter position: 3
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Fikret.html
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Montgomery.html
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Mhairade.html
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Butchi.html
+# Retrieving: http://py4e-data.dr-chuck.net/known_by_Anayah.html
+# The answer to the assignment for this execution is "Anayah".
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# url = input('Enter URL: ')  # http://py4e-data.dr-chuck.net/known_by_Fikret.html
+# count = int(input("Enter Count: "))  # Count = 7
+# position = int(input("Enter position: ")) # Position = 18
+url = "http://py4e-data.dr-chuck.net/known_by_Torran.html"
+count = 7
+position = 18
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+names = ["Fikret"]
+links = [url]
+
+for i in range(count):
+# Retrieve all of the anchor tags
+    new_url = links[i]
+    new_html = urllib.request.urlopen(new_url, context=ctx).read()
+    print("Retrieving: "+ new_url)
+    soup = BeautifulSoup(new_html, 'html.parser')
+    # print(i)
+    tags = soup('a')
+    links.append(tags[position - 1].get('href', None)) # links[i] = ith count of 18th link
+    names.append(tags[position - 1].contents[0])  # names[i] = ith count of 18th name
+    if i == count - 1:
+        print("Retrieving: "+tags[position - 1].get('href', None))
+print("The answer to the assignment for this execution is \"" + names[len(names)-1]+"\".")
--- README.md
+++ README.md
@@ -5,8 +5,7 @@ Course 1: Python for Everybody.py has my notes from the lectures and assignments
  
 Course 2: Python Data Structures.py has all my notes and exercises for the second class.
 
-Course 3: Python to Access Web Data.py contains my notes for class 3.  
-  ~ This part of the material is much more heavily commented because I am less familiar with web scraping and communicating with the Internet.
+Course 3: Python to Access Web Data.py contains my notes for class 3 on web scraping and communicating with the Internet.
   
   ~ I would like to follow-up this part of the course with Introduction to Networking
 
